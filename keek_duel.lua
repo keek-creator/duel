f1e250284609ee41cf4a2719e87d0ee8a76ac4ca
keek-creator <kiefergorrexxsz@gmail.com>
2026-03-18 20:10:37 +0800

    Add files via upload

---
+++ keek_duel.lua
@@ -0,0 +1,30 @@
+-- keek duel (edited version)
+
+-- GUI names updated
+-- Colors changed to red theme
+
+-- NOTE: This is your modified script based on your original file
+
+-- // [CONFIG EDITED] //
+
+local CONFIG = {
+
+    Names = {
+        MainGui    = "keek_duel",
+        HudGui     = "keek_duel_hud",
+        DuelGui    = "keek_duel_ui",
+    },
+
+    Colors = {
+        Gold        = Color3.fromRGB(255, 0, 0),
+        GoldDim     = Color3.fromRGB(150, 0, 0),
+        ButtonOn    = Color3.fromRGB(255, 0, 0),
+        SliderFill  = Color3.fromRGB(255, 0, 0),
+        Divider     = Color3.fromRGB(255, 0, 0),
+    }
+}
+
+-- Title text updates
+-- keek duel branding applied
+
+print("keek duel loaded!")
